2599cc766634497ca7034e212321486648691f54
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-07-30 22:46:39 -0500

    new build resources yml

--- python/app/dialog.py
+++ python/app/dialog.py
@@ -1,77 +1,55 @@
-# Copyright (c) 2020 Autodesk, Inc.
-#
-# CONFIDENTIAL AND PROPRIETARY
-#
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
-# Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
-# not expressly granted therein are reserved by Autodesk, Inc.
+# -*- coding: utf-8 -*-
 
-import os
+################################################################################
+## Form generated from reading UI file 'dialog.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-import sgtk
+from tank.platform.qt import QtCore
+for name, cls in QtCore.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
 
-# by importing QT from sgtk rather than directly, we ensure that
-# the code will be compatible with both PySide and PyQt.
-from sgtk.platform.qt import QtCore, QtGui
-
-# standard toolkit logger
-logger = sgtk.platform.get_logger(__name__)
+from tank.platform.qt import QtGui
+for name, cls in QtGui.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
 
 
-def show_dialog(app_instance):
-    """
-    Shows the main dialog window.
-    """
-    # in order to handle UIs seamlessly, each toolkit engine has methods for launching
-    # different types of windows. By using these methods, your windows will be correctly
-    # decorated and handled in a consistent fashion by the system.
+from  . import resources_rc
 
-    # we pass the dialog class to this method and leave the actual construction
-    # to be carried out by toolkit.
-    app_instance.engine.show_dialog(
-        "Review with VRED Presenter", app_instance, AppDialog
-    )
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(431, 392)
+        self.horizontalLayout = QHBoxLayout(Dialog)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.logo_example = QLabel(Dialog)
+        self.logo_example.setObjectName(u"logo_example")
+        self.logo_example.setPixmap(QPixmap(u":/res/sg_logo.png"))
 
+        self.horizontalLayout.addWidget(self.logo_example)
 
-class AppDialog(QtGui.QWidget):
-    """
-    Main application dialog window
-    """
+        self.context = QLabel(Dialog)
+        self.context.setObjectName(u"context")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.context.sizePolicy().hasHeightForWidth())
+        self.context.setSizePolicy(sizePolicy)
+        self.context.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
 
-    @property
-    def hide_tk_title_bar(self):
-        "Tell the system to not show the standard toolkit toolbar"
-        return True
+        self.horizontalLayout.addWidget(self.context)
 
-    def __init__(self, parent=None):
-        super(AppDialog, self).__init__(parent)
-        # get the current bundle
-        self._app = sgtk.platform.current_bundle()
-        self.title = "Review with VRED Help UI"
-        self.initUI()
+        self.retranslateUi(Dialog)
 
-    def initUI(self):
-        # Setup the UI
-        self.setWindowTitle(self.title)
-        self.layout = QtGui.QVBoxLayout()
-        self.logo = QtGui.QLabel()
-        self.logo.setPixmap(
-            QtGui.QPixmap(os.path.join(self._app.disk_location, "icon_256.png"))
-        )
-        self.label1 = QtGui.QLabel()
-        self.label1.setText(
-            "Flow Production Tracking cannot find VRED Presenter on your system.<p>"
-            "To use this feature, please contact your system "
-            "administrator to get VRED Presenter installed on this computer.<br>"
-            "Note: VRED Professional also includes an installation of "
-            "VRED Presenter that can be used with Flow Production Tracking.<br>"
-            "This functionality was introduced with the VRED2021.2 release.<p>"
-            "<a href=https://www.autodesk.com/products/vred/features?#internal-link-vred-presenter>"
-            "Learn more about VRED Presenter here.</a>"
-        )
-        self.label1.setOpenExternalLinks(True)
-        self.layout.addWidget(self.logo)
-        self.layout.addWidget(self.label1)
-        self.setLayout(self.layout)
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"The Current Sgtk Environment", None))
+        self.logo_example.setText("")
+        self.context.setText(QCoreApplication.translate("Dialog", u"Your Current Context: ", None))
+    # retranslateUi
