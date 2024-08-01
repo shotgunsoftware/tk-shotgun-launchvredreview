8c218aac4c0872a24aadf4387a1d4391c884d49f
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-07-31 23:48:57 -0500

    nlck formatting

--- python/app/dialog.py
+++ python/app/dialog.py
@@ -9,47 +9,58 @@
 ################################################################################
 
 from tank.platform.qt import QtCore
+
 for name, cls in QtCore.__dict__.items():
-    if isinstance(cls, type): globals()[name] = cls
+    if isinstance(cls, type):
+        globals()[name] = cls
 
 from tank.platform.qt import QtGui
+
 for name, cls in QtGui.__dict__.items():
-    if isinstance(cls, type): globals()[name] = cls
+    if isinstance(cls, type):
+        globals()[name] = cls
 
 
-from  . import resources_rc
+from . import resources_rc
+
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
-            Dialog.setObjectName(u"Dialog")
+            Dialog.setObjectName("Dialog")
         Dialog.resize(431, 392)
         self.horizontalLayout = QHBoxLayout(Dialog)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setObjectName("horizontalLayout")
         self.logo_example = QLabel(Dialog)
-        self.logo_example.setObjectName(u"logo_example")
-        self.logo_example.setPixmap(QPixmap(u":/res/sg_logo.png"))
+        self.logo_example.setObjectName("logo_example")
+        self.logo_example.setPixmap(QPixmap(":/res/sg_logo.png"))
 
         self.horizontalLayout.addWidget(self.logo_example)
 
         self.context = QLabel(Dialog)
-        self.context.setObjectName(u"context")
+        self.context.setObjectName("context")
         sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.context.sizePolicy().hasHeightForWidth())
         self.context.setSizePolicy(sizePolicy)
-        self.context.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.context.setAlignment(Qt.AlignLeading | Qt.AlignLeft | Qt.AlignVCenter)
 
         self.horizontalLayout.addWidget(self.context)
 
         self.retranslateUi(Dialog)
 
         QMetaObject.connectSlotsByName(Dialog)
+
     # setupUi
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"The Current Sgtk Environment", None))
+        Dialog.setWindowTitle(
+            QCoreApplication.translate("Dialog", "The Current Sgtk Environment", None)
+        )
         self.logo_example.setText("")
-        self.context.setText(QCoreApplication.translate("Dialog", u"Your Current Context: ", None))
+        self.context.setText(
+            QCoreApplication.translate("Dialog", "Your Current Context: ", None)
+        )
+
     # retranslateUi
